242e9759c56bee6022859d9281fabc8e6a1b63bf
jacob.b.schroder <jacob.b.schroder@bc1f6894-eb43-0410-9588-45a1cfa25dd0>
2008-11-17 22:52:27 +0000

    1st installment of Krylov subpackage

--- pyamg/krylov/__init__.py
+++ pyamg/krylov/__init__.py
@@ -3,8 +3,12 @@
 
 from info import __doc__
 
-from krylov import *
+from gmres import *
+from fgmres import *
 from cg import *
+from cgnr import *
+from cgne import *
+#from bicgstab import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from pyamg.testing import Tester
--- pyamg/krylov/cg.py
+++ pyamg/krylov/cg.py
@@ -1,128 +1,26 @@
 from numpy import asmatrix, asarray, zeros, dot
 from numpy import asanyarray, asarray, asmatrix, array, matrix, zeros
-from pyamg.util.utils import norm
+from numpy.random import random
+from scipy.sparse.linalg.isolve.utils import make_system
 from scipy.linalg import solve as direct_solve
 from scipy.sparse.linalg.interface import aslinearoperator, LinearOperator
-from numpy.random import random
+from pyamg.util.utils import norm
 from warnings import warn
 import time
 
-_coerce_rules = {('f','f'):'f', ('f','d'):'d', ('f','F'):'F',
-                 ('f','D'):'D', ('d','f'):'d', ('d','d'):'d',
-                 ('d','F'):'D', ('d','D'):'D', ('F','f'):'F',
-                 ('F','d'):'D', ('F','F'):'F', ('F','D'):'D',
-                 ('D','f'):'D', ('D','d'):'D', ('D','F'):'D',
-                 ('D','D'):'D'}
-
-def coerce(x,y):
-    if x not in 'fdFD':
-        x = 'd'
-    if y not in 'fdFD':
-        y = 'd'
-    return _coerce_rules[x,y]
-
-def id(x):
-    return x
-def make_system(A, M, x0, b, xtype=None):
-    """Make a linear system Ax=b
- 
-    Parameters
-    ----------
-    A : LinearOperator
-        sparse or dense matrix (or any valid input to aslinearoperator)
-    M : {LinearOperator, Nones}
-        preconditioner
-        sparse or dense matrix (or any valid input to aslinearoperator)
-    x0 : {array_like, None}
-        initial guess to iterative method
-    b : array_like
-        right hand side
-    xtype : {'f', 'd', 'F', 'D', None}
-        dtype of the x vector
- 
-    Returns
-    -------
-    (A, M, x, b, postprocess)
-        A : LinearOperator
-            matrix of the linear system
-        M : LinearOperator
-            preconditioner
-        x : rank 1 ndarray
-            initial guess
-        b : rank 1 ndarray
-            right hand side
-        postprocess : function
-            converts the solution vector to the appropriate
-            type and dimensions (e.g. (N,1) matrix)
-    """
-    A_ = A
-    A = aslinearoperator(A)
- 
-    if A.shape[0] != A.shape[1]:
-        raise ValueError('expected square matrix (shape=%s)' % shape)
- 
-    N = A.shape[0]
- 
-    b = asanyarray(b)
- 
-    if not (b.shape == (N,1) or b.shape == (N,)):
-        raise ValueError('A and b have incompatible dimensions')
- 
-    if b.dtype.char not in 'fdFD':
-        b = b.astype('d') # upcast non-FP types to double
- 
-    def postprocess(x):
-        if isinstance(b,matrix):
-            x = asmatrix(x)
-        return x.reshape(b.shape)
- 
-    if xtype is None:
-        if hasattr(A,'dtype'):
-            xtype = A.dtype.char
-        else:
-            xtype = A.matvec(b).dtype.char
-        xtype = coerce(xtype, b.dtype.char)
-    else:
-        warn('Use of xtype argument is deprecated. '\
-                'Use LinearOperator( ... , dtype=xtype) instead.',\
-                DeprecationWarning)
-        if xtype == 0:
-            xtype = b.dtype.char
-        else:
-            if xtype not in 'fdFD':
-                raise ValueError, "xtype must be 'f', 'd', 'F', or 'D'"
- 
-    b = asarray(b,dtype=xtype) #make b the same type as x
- 
-    if x0 is None:
-        x = zeros(N, dtype=xtype)
-    else:
-        x = array(x0, dtype=xtype)
-        if not (x.shape == (N,1) or x.shape == (N,)):
-            raise ValueError('A and x have incompatible dimensions')
-        x = x.ravel()
- 
-    # process preconditioner
-    if M is None:
-        if hasattr(A_,'psolve'):
-            psolve = A_.psolve
-        else:
-            psolve = id
-        if hasattr(A_,'rpsolve'):
-            rpsolve = A_.rpsolve
-        else:
-            rpsolve = id
-        M = LinearOperator(A.shape, matvec=psolve, rmatvec=rpsolve, dtype=A.dtype)
-    else:
-        M = aslinearoperator(M)
-        if A.shape != M.shape:
-            raise ValueError('matrix and preconditioner have different shapes')
- 
-    return A, M, x, b, postprocess
-
-
 def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
     """
+    Parameters
+    ----------
+    
+    Returns
+    -------    
+    
+    Notes
+    -----
+
+    References
+    ----------
     PCG from Saad
     """
 
@@ -131,8 +29,6 @@ def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
     x = x.ravel()
     b = b.ravel()
 
-    matvec = A.matvec
-    psolve = M.matvec
     T0a = time.time()-t
     
     t = time.time()
@@ -144,7 +40,7 @@ def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
     doneiterating = False
     iter = 0
 
-    r = b - matvec(x0)
+    r = b - A*x0
 
     normr0 = norm(r)
 
@@ -153,10 +49,10 @@ def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
     T0b = time.time()-t
 
     t = time.time()
-    z = psolve(r)
+    z = M*r
 
     p = z.copy()
-    Ap = matvec(p)
+    Ap = A*p
 
     T0c = time.time()-t
     print 'T0a : %g ms'%(T0a*1000)
@@ -196,7 +92,7 @@ def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
         t=time.time()
         z = znew
         r = rnew
-        Ap = matvec(p)
+        Ap = A*p
         iter += 1
         T6 += time.time()-t
 
@@ -219,7 +115,7 @@ def cg(A, b, x0=None, tol=1e-5, maxiter=None, M=None, callback=None):
     print 'T7 : %g ms'%(T7*1000)
     print 'total : %g ms'%((T0a+T0b+T0c+T1+T2+T3+T4+T5+T6+T7)*1000)
     print 'iterations: %d\n\n'%(iter)
-    return x
+    return postprocess(x)
 
 if __name__ == '__main__':
     # from numpy import diag
@@ -249,3 +145,5 @@ if __name__ == '__main__':
     print '\n%s took %0.3f ms' % ('linalg cg', (t2-t1)*1000.0)
     print 'norm = %g'%(norm(b - A*y[0]))
     print 'info flag = %d'%(y[1])
+
+    
